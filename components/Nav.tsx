b28b5da3cc4079321c3a669a277f896915b527c3
icaro_albar <icaro.albar@live.com>
2023-01-13 20:50:12 -0300

    Tradutor no Nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-
 import Image from "next/image";
 
-export const Nav = () => {
+export const Link = (props: any) => {
+  return <a className="px-5 py-5 border-secondary-10 border-b hover:bg-secondary-10 md:hover:bg-transparent md:px-0 md:py-0 md:border-none font-bold uppercase duration-300 text-sm md:hover:text-secondary-10" href={props.href}>{props.value}</a>
+}
+
+export const Nav = (props: any) => {
 
   const [mobileOpen, setMobileOpen] = useState(false)
 
-  const links = 'font-bold uppercase duration-300 hover:text-secondary-10 text-sm'
-  const linkMobile = 'px-5 font-bold uppercase duration-300 hover:bg-secondary-10 text-sm border-b py-5 border-secondary-10'
   const toogle = 'w-7 h-1 bg-white mb-1 transition-all duration-300 ease-in-out'
 
   return (
@@ -23,23 +24,16 @@ export const Nav = () => {
 
         /></a>
       <nav className={`bg-primary-10 text-white flex items-center md:justify-end px-12 py-16 md:px-24 border-b-4 border-secondary-10 ${mobileOpen ? 'h-screen md:h-28 duration-200' : "h-28 duration-200"}`}>
-        <ul className='hidden md:flex gap-x-10'>
-          <li><a className={links} href="#">quem somos</a></li>
-          <li><a className={links} href="#">serviços</a></li>
-          <li><a className={links} href="#">contato</a></li>
-        </ul>
+
+        <div className={`${mobileOpen ? 'md:hidden flex flex-col w-full border-t border-secondary-10' : 'hidden md:flex gap-x-10' }`}>
+          {props.children}
+        </div>
 
         <button className="md:hidden absolute top-14 right-10" onClick={() => setMobileOpen((currentValue) => !currentValue)}>
           <div className={`${toogle} ${mobileOpen ? "rotate-45 translate-y-2" : ""}`}></div>
           <div className={`${toogle} ${mobileOpen ? "hidden" : ""}`}></div>
           <div className={`${toogle} ${mobileOpen ? "-rotate-45" : ""}`}></div>
         </button>
-
-        <div className={`md:hidden flex flex-col w-full border-t border-secondary-10 ${mobileOpen ? "flex" : "hidden"}`}>
-          <a className={linkMobile} href="#">quem somos</a>
-          <a className={linkMobile} href="#">serviços</a>
-          <a className={linkMobile} href="#">contato</a>
-        </div>
 
       </nav>
     </>
