8480b489a69c07eedc6e5a3e29f7ecdf0a029ea0
icaro_albar <icaro.albar@live.com>
2023-01-13 13:59:44 -0300

    menu reconfigurado

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,29 +1,49 @@
 import { useState } from 'react'
 
+import Image from "next/image";
+
 export const Nav = () => {
 
-    const [mobileOpen, setMobileOpen] = useState(false)
+  const [mobileOpen, setMobileOpen] = useState(false)
 
-    const link = "text-white mx-5 uppercase hover:text-red-500"
-    const linha = 'w-8 h-1 bg-white mb-1'
+  const links = 'font-bold uppercase duration-300 hover:text-secondary-10 text-sm'
+  const linkMobile = 'px-5 font-bold uppercase duration-300 hover:bg-secondary-10 text-sm border-b py-5 border-secondary-10'
+  const toogle = 'w-7 h-1 bg-white mb-1 transition-all duration-300 ease-in-out'
 
-    return (
-        <header className={`duration-700 w-full h-20 bg-blue-900 ${mobileOpen ? 'h-screen' : ''}`}>
-            <nav className={` flex items-start pt-7 md:items-center justify-between px-20 `}>
-                <a href='#'>Logo</a>
-                <ul className="hidden md:flex">
-                    <li className={link}><a href="#">portfólio</a></li>
-                    <li className={link}><a href="#">quem somos</a></li>
-                    <li className={link}><a href="#">contato</a></li>
-                </ul>
-                <button className="md:hidden" onClick={() => setMobileOpen((currentValue) => !currentValue)}>
-                    <div className={linha}></div>
-                    <div className={linha}></div>
-                    <div className={linha}></div>
-                </button>
-            </nav>
-        </header>
-    )
+  return (
+    <>
+      <a href="./"
+        className="flex absolute left-20 top-4"
+      >
+        <Image
+          src="/logoAga.svg"
+          width={150}
+          height={150}
+          alt="Logo da AGÁ Empreendimentos"
+
+        /></a>
+      <nav className={`bg-primary-10 text-white flex items-center md:justify-end px-12 py-16 md:px-24 border-b-4 border-secondary-10 ${mobileOpen ? 'h-screen md:h-28 duration-200' : "h-28 duration-200"}`}>
+        <ul className='hidden md:flex gap-x-10'>
+          <li><a className={links} href="#">quem somos</a></li>
+          <li><a className={links} href="#">serviços</a></li>
+          <li><a className={links} href="#">contato</a></li>
+        </ul>
+
+        <button className="md:hidden absolute top-14 right-10" onClick={() => setMobileOpen((currentValue) => !currentValue)}>
+          <div className={`${toogle} ${mobileOpen ? "rotate-45 translate-y-2" : ""}`}></div>
+          <div className={`${toogle} ${mobileOpen ? "hidden" : ""}`}></div>
+          <div className={`${toogle} ${mobileOpen ? "-rotate-45" : ""}`}></div>
+        </button>
+
+        <div className={`md:hidden flex flex-col w-full border-t border-secondary-10 ${mobileOpen ? "flex" : "hidden"}`}>
+          <a className={linkMobile} href="#">quem somos</a>
+          <a className={linkMobile} href="#">serviços</a>
+          <a className={linkMobile} href="#">contato</a>
+        </div>
+
+      </nav>
+    </>
+  )
 }
 
 export default Nav
