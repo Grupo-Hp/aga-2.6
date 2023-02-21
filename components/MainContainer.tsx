8b7abbe041d3f967ca7d6952ae3006475a3b7ff6
icaroalbar <icaro.albar@live.com>
2023-02-21 12:27:31 -0300

    render fix in nav

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -1,4 +1,4 @@
-import { Nav, LinkNav } from '../components/Nav'
+import { Nav } from '../components/Nav'
 import { Footer } from "./Footer"
 
 interface Props {
@@ -8,11 +8,7 @@ interface Props {
 export const MainContainer = ({ children }: Props) => {
     return (
         <>
-            <Nav>
-                <LinkNav href="./contact" value="quem somos" />
-                <LinkNav href="./contact" value="serviços" />
-                <LinkNav href="./contact" value="contato" />
-            </Nav>
+            <Nav />
             {children}
             <Footer />
         </>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,29 +2,18 @@ import { ReactElement, useState } from 'react'
 import Image from "next/image";
 import Link from 'next/link';
 
-interface Props {
-  href: string
-  value: string
-}
-
-interface INav {
-  children: React.ReactNode;
-}
-
-export const LinkNav = ({value, href}: Props): ReactElement => {
-  return <Link className="px-5 py-5 border-secondary-10 border-b hover:bg-secondary-10 md:hover:bg-transparent md:px-0 md:py-0 md:border-none font-bold uppercase duration-300 text-sm md:hover:text-secondary-10" href={href}>{value}</Link>
-}
-
-export const Nav = ({children}: INav): ReactElement => {
+export const Nav = (): ReactElement => {
 
   const [mobileOpen, setMobileOpen] = useState(false)
 
-  const toogle = 'w-7 h-1 bg-white mb-1 transition-all duration-300 ease-in-out'
+  const toogle = "w-7 h-1 bg-white mb-1 transition-all duration-300 ease-in-out"
+  const linkStyle = "px-5 py-5 border-secondary-10 border-b hover:bg-secondary-10 md:hover:bg-transparent md:px-0 md:py-0 md:border-none font-bold uppercase duration-300 text-sm md:hover:text-secondary-10"
 
   return (
     <>
-      <Link href="./"  
+      <Link href="./"
         className="flex absolute left-20 top-4"
+        onClick={() => setMobileOpen(false)}
       >
         <Image
           src="./logoAga.svg"
@@ -35,8 +24,10 @@ export const Nav = ({children}: INav): ReactElement => {
         /></Link>
       <nav className={`bg-primary-10 text-white flex scroll-m-0 items-center md:justify-end px-12 py-16 md:px-24 border-b-4 border-secondary-10 ${mobileOpen ? 'h-screen md:h-28 duration-200' : "h-28 duration-200"}`}>
 
-        <div className={`${mobileOpen ? 'md:hidden flex flex-col w-full border-t border-secondary-10' : 'hidden md:flex gap-x-10' }`}>
-          {children}
+        <div className={`${mobileOpen ? 'md:hidden flex flex-col w-full border-t border-secondary-10' : 'hidden md:flex gap-x-10'}`}>
+          <Link className={linkStyle} onClick={() => setMobileOpen(false)} href='/contact'>quem somos</Link>
+          <Link className={linkStyle} onClick={() => setMobileOpen(false)} href='/contact'>serviços</Link>
+          <Link className={linkStyle} onClick={() => setMobileOpen(false)} href='/contact'>contato</Link>
         </div>
 
         <button className="md:hidden absolute top-14 right-10" onClick={() => setMobileOpen((currentValue) => !currentValue)}>
